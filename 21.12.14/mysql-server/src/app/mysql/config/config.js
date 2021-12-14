e3de9a49274439371cedc60a07985d4bcbd706a6
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2021-12-15 08:55:07 +0900

    login

---
+++ 21.12.14/mysql-server/src/app/mysql/config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    host: "[host]",
+    username: "[username]",
+    password: "[password]",
+    db: "[dbname]",
+    dialect: "[dbtype]",
+    pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
+};
