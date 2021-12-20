3bf01cef5c63f87a339334404aed8a93b794e158
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2021-12-20 23:15:40 +0900

    LOGIN JS

---
+++ 실습/21.12.20/models/user.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize')
+
+module.exports = class User extends Sequelize.Model{
+    static init(sequelize){
+        return super.init({
+            name :{
+                type : Sequelize.STRING(20),
+                allowNull: false,
+                unique:true,
+            },
+            age :{
+                type : Sequelize.INTEGER.UNSIGNED,
+                allowNull: false,
+            }
+        })
+    }
+}
